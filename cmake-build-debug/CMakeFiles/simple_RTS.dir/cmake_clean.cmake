32958b70555d477a2a81aac2e7d16c7415ebb79a
Oleg Schwann <cup.of.software.code@gmail.com>
2017-11-19 14:25:08 +0300

    минимальная компилящаяся часть. Только исправит абсолютный путь линковки на свой в CMakeLists.txt

---
+++ cmake-build-debug/CMakeFiles/simple_RTS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_RTS.dir/main.cpp.o"
+  "simple_RTS.pdb"
+  "simple_RTS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_RTS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,37 +1,50 @@
-#include <SFML/Network.hpp>
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#define PORT 2000
+/* Клиент игры Танчики.
+   Это сетевая игра, клиент только
+    устанавливает соединнение
+    посылает сигналы ввода пользователя вида "нажата кнопка ← ↑ → ↓ ␣"
+    структура данных -
+        имя пользователя,
+        время нажатия,
+        код действия.
+    собственно, пользователю доступно только перемещение и стрельба
+    принимает номер, кординаты и статус игровых объектов
+    ! - уточнить структуру данных, принимаемую
+    применяет все пришедшие изменения
+    отрисовывает карту, врагов, обоих игроков
+    повторяет(ждёт следующего тика)
+*/
 
-using namespace sf;
+#include <SFML/Graphics.hpp>
+#include <SFML/System/Time.hpp>
+#include <iostream>
 
 int main()
-{   IpAddress ip = IpAddress::getLocalAddress();
-    TcpSocket socket;
-    Packet packet;
-    TcpListener listener;
-    listener.listen(PORT);
-    listener.accept(socket);
-    char data[20];
-    size_t received;
-    socket.receive(data, sizeof(data), received);
-    Font font;
-    font.loadFromFile("/home/mrfalcon/CLionProjects/Dandy_Tanks/arial.ttf");
-    Text text(data, font, 20);
-    RenderWindow window(VideoMode(200, 200), "nettest");
-    while(window.isOpen())
+{
+    // все подготовительные действия
+    // минимальная длительность игрового цикла
+    sf::Time cycle_time = sf::seconds(1);//0.02f);
+    sf::RenderWindow window(sf::VideoMode(480, 416), "simple rts");
+
+    sf::Clock clock;
+    while (window.isOpen())
     {
-        Event event;
-        while(window.pollEvent(event))
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-            if(event.type == Event::Closed)
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) { };
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){ };
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){ };
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){ };
+            if (event.type == sf::Event::Closed)
                 window.close();
         }
 
         window.clear();
-        window.draw(text);
         window.display();
+        sf::Time elapsed_time = clock.getElapsedTime() % cycle_time;
+        std::cout << elapsed_time.asMicroseconds() << std::endl;
+        sf::sleep(cycle_time - elapsed_time);
     }
+
     return 0;
 }
-
