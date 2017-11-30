887918dcc79f6983e057198a9bb36bb9d6c65160
Oleg Schwann <cup.of.software.code@gmail.com>
2017-11-30 11:02:58 +0300

    мастер очищен для слияния

--- main.cpp
+++ main.cpp
@@ -1,37 +1,7 @@
-#include <SFML/Network.hpp>
 #include <SFML/Graphics.hpp>
-#include <iostream>
-#define PORT 2000
 
-using namespace sf;
-
-int main()
-{   IpAddress ip = IpAddress::getLocalAddress();
-    TcpSocket socket;
-    Packet packet;
-    TcpListener listener;
-    listener.listen(PORT);
-    listener.accept(socket);
-    char data[20];
-    size_t received;
-    socket.receive(data, sizeof(data), received);
-    Font font;
-    font.loadFromFile("/home/mrfalcon/CLionProjects/Dandy_Tanks/arial.ttf");
-    Text text(data, font, 20);
-    RenderWindow window(VideoMode(200, 200), "nettest");
-    while(window.isOpen())
-    {
-        Event event;
-        while(window.pollEvent(event))
-        {
-            if(event.type == Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(text);
-        window.display();
-    }
+int main(){
+   
     return 0;
 }
 
